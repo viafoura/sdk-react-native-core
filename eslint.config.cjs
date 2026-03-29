f74bc1cc9d298a14aa66e6d19c9724463e7ca9ee
Martin De Simone <martindesimone481@gmail.com>
2026-03-29 19:55:33 -0300

    Create eslint.config.cjs

---
+++ eslint.config.cjs
@@ -0,0 +1,15 @@
+const js = require("@eslint/js");
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = [
+  ...compat.extends("universe/native", "universe/web"),
+  {
+    ignores: ["build"],
+  },
+];
