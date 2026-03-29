925ea916467edacf0dbe4c66738122c9c4b76ec5
Martin De Simone <martindesimone481@gmail.com>
2026-03-29 19:57:08 -0300

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,5 +1,6 @@
 const js = require("@eslint/js");
 const { FlatCompat } = require("@eslint/eslintrc");
+const eslintPluginNode = require("eslint-plugin-node");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -11,5 +12,10 @@ module.exports = [
   ...compat.extends("universe/native", "universe/web"),
   {
     ignores: ["build"],
+    // eslint-plugin-node is not compatible with ESLint 9 (context.getScope removal).
+    // Disable node/* rules until the upstream config is updated.
+    rules: Object.fromEntries(
+      Object.keys(eslintPluginNode.rules).map((rule) => [`node/${rule}`, "off"])
+    ),
   },
 ];
